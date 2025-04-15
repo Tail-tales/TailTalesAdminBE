b87e670f686001d1b082c9b1e0e1da189cf979fd
eggzuxi <eggzuxi99@gmail.com>
2025-04-15 17:13:24 +0900

    :fire: delete: 유효성 검사 메시지 삭제 프론트엔드에서 처리함

--- src/main/java/com/tailtales/backend/domain/admin/dto/AdminCreateRequestDto.java
+++ src/main/java/com/tailtales/backend/domain/admin/dto/AdminCreateRequestDto.java
@@ -14,24 +14,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AdminCreateRequestDto {
 
-    @NotBlank(message = "이름을 입력해주세요.")
+    @NotBlank
     private String name;
 
-    @NotBlank(message = "아이디를 입력해주세요.")
+    @NotBlank
     private String adminId;
 
-    @NotBlank(message = "비밀번호를 입력해주세요")
-    @Pattern(
-            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+=-]).{8,20}$",
-            message = "비밀번호는 8~20자, 영문/숫자/특수문자를 포함해야 합니다."
-    )
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+=-]).{8,20}$")
     private String password;
 
-    @Pattern(regexp = "^01[0-9]\\d{7,8}$", message = "올바른 휴대폰 번호를 입력해주세요.")
+    @Pattern(regexp = "^01[0-9]\\d{7,8}$")
     private String contact;
 
     @Email
-    @NotBlank(message = "이메일을 입력해주세요")
+    @NotBlank
     private String email;
 
 }
