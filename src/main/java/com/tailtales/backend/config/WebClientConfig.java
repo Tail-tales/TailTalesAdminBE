c5281734c651b75f7d7eda44a4126c10f8a6683b
eggzuxi <eggzuxi99@gmail.com>
2025-04-22 16:55:39 +0900

    :sparkles: feat: Webclient 환경설정

---
+++ src/main/java/com/tailtales/backend/config/WebClientConfig.java
@@ -0,0 +1,29 @@
+package com.tailtales.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient customWebClient() {
+
+        HttpClient httpClient = HttpClient.create()
+                .wiretap(true);
+
+        return WebClient.builder()
+                .baseUrl("http://localhost:8082")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+
+    }
+
+}
